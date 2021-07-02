d30af478a58bf1c7baa9fef41777db999ac39ef0
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-07-02 14:18:51 +0530

    Create EvenOdd_IfElse.C
    If else program to check whether a number is even or odd.

---
+++ EvenOdd_IfElse.C
@@ -0,0 +1,15 @@
+//If else program to check whether a number is even or odd.
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a number:\n");
+    scanf("%d", &a);
+    if(a%2==00){
+    printf("%d is even\n", a);
+    }
+    else{
+        printf("%d is odd\n", a);
+        return 0;
+    }
+}
