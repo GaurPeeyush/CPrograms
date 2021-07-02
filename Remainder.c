98e67fbe4e5537833d8b8eec98890b5983c5ea7b
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-07-02 14:13:22 +0530

    Create Remainder.c
    C program to find the remainder when number is divided by 2

---
+++ Remainder.c
@@ -0,0 +1,10 @@
+//C program to find the remainder when number is divided by 2
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the number\n");
+    scanf("%d", &num);
+    printf("Remainder when number is divded by 2 is: %d\n", num%2);
+    return 0;
+}
