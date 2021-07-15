bef68b8539a3366352b139c4f6670d1569c4a62e
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-07-15 22:26:22 +0530

    Add files via upload

---
+++ Practice questions/Question1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int phy, chem, maths;
+     printf("Enter Physics marks\n");
+    scanf("%d", &phy);
+     printf("Enter Chemistry marks\n");
+    scanf("%d", &chem);
+     printf("Enter Maths marks\n");
+    scanf("%d", &maths);
+     float sum;
+     sum = (phy + chem + maths) / 3;
+    if ((sum < 40) || phy < 33 || chem < 33 || maths < 33)
+    {
+        printf("Your total percentage is %f and you are failed!", sum);
+    }
+    else
+    {
+        printf("Your total percentage is %f and you are passed!", sum);
+    }
+    return 0;
+}
