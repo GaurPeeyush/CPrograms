288f3992e6f99a8cb6848a31f33c9eca5a823725
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-08-15 00:26:56 +0530

    Update ASCII.c

--- ASCII.c
+++ ASCII.c
@@ -1,1 +1,15 @@
-
+//To check whether the entered character is in lower case or not
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter the character\n");
+    scanf("%c", &ch);
+    if(ch>=97 && ch<=122){
+        printf("The entered character is in lower case\n");
+    }
+    else{
+        printf("The entered character is not in lower case\n");
+    }
+    return 0;
+}
