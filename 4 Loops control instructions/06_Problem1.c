db6c0412aa822ad20b5d9346d3da40a57316d31f
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-08-20 00:50:38 +0530

    Update 06_Problem1.c

--- 4 Loops control instructions/06_Problem1.c
+++ 4 Loops control instructions/06_Problem1.c
@@ -11,3 +11,16 @@ int main(){
       }while(a<11);
 return 0;
 }
+/* Using for loop: 
+#include<stdio.h>
+int main(){
+   int a, n, result;
+   printf("Enter the number n:\n");
+   scanf("%d", &n);
+     for(a = 1; a < 11; a++){
+     result = a*n;
+     printf("%d*%d = %d\n", n, a, result);
+     }
+return 0;
+}
+*/
