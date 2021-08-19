5471c31ae3caf2cdb6e1cd0516539adf19da0188
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-08-20 01:39:55 +0530

    Update 06_Problem2.c

--- 4 Loops control instructions/06_Problem2.c
+++ 4 Loops control instructions/06_Problem2.c
@@ -1,1 +1,12 @@
-
+//program to print the sum of natural numbers upto n
+#include<stdio.h>
+int main(){
+    int i, n, sum = 0;
+    printf("Enter the number n:\n");
+    scanf("%d", &n);
+    for(i = 0; i<=n; i++){
+        sum +=i;
+    }
+    printf("The sum of natural numbers upto %d is %d\n", n, sum);
+    return 0;
+}
