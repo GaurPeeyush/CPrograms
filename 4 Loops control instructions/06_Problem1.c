023f48de4fd797de84209e101661e5a35152c5f3
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-08-20 00:42:52 +0530

    Update 06_Problem1.c

--- 4 Loops control instructions/06_Problem1.c
+++ 4 Loops control instructions/06_Problem1.c
@@ -1,1 +1,13 @@
-
+//print multiplication table of a given number n
+#include<stdio.h>
+int main(){
+  int a = 1, n, result;
+  printf("Enter a number n:\n");
+  scanf("%d", &n);
+    do{
+      result = a*n;
+      printf("%d*%d = %d\n",n, a, result);
+      a++;
+      }while(a<11);
+return 0;
+}
