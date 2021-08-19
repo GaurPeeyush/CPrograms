058cf232b24c3ecf954c73b7a918511cf388ecef
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-08-19 22:10:09 +0530

    Update 03_ForLoop.c

--- 4 Loops control instructions/03_ForLoop.c
+++ 4 Loops control instructions/03_ForLoop.c
@@ -1,1 +1,12 @@
-
+//program to print n natural numbers using for loop
+#include<stdio.h>
+int main(){
+    int n, a;
+    printf("Enter the value of n: \n");
+    scanf("%d", &n);
+    for(a = 0; a < n; a++){
+    printf("%d", a+1);
+    printf("\n");
+    }
+    return 0;
+}
