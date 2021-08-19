a4cbd9aff41baaf55bb2e17582e44db1f02ce8f0
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-08-20 02:22:19 +0530

    Update 06_Problem4.c

--- 4 Loops control instructions/06_Problem4.c
+++ 4 Loops control instructions/06_Problem4.c
@@ -1,1 +1,22 @@
-
+#include <stdio.h>
+int main()
+{
+    int n, i, prime = 1;
+    printf("Enter the number n:\n");
+    scanf("%d", &n);
+    for (i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            prime = 0;
+            break;
+        }
+    }
+    if (prime==0 && n!=2){
+        printf("%d is not a prime number\n", n);
+    }
+    else{
+        printf("%d is a prime number\n", n);
+    }
+    return 0;
+}
