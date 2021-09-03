85ae248849835b58d017ae71632bb2df63b76af1
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-09-03 19:22:37 +0530

    Update RandomVariable.c

--- RandomVariable.c
+++ RandomVariable.c
@@ -1,1 +1,10 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+  int number;
+    srand(time(0));
+    number = rand()%100 + 1; //Generates random number between 10 and 100
+    printf("The number is %d\n", number);
+return 0;
+}
