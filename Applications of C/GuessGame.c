c1891140684a5df27f2641149877ce49ca575091
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-09-03 19:37:31 +0530

    Update GuessGame.c

--- Applications of C/GuessGame.c
+++ Applications of C/GuessGame.c
@@ -1,1 +1,24 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+    int number, guess, nguess=1;
+    srand(time(0));
+    number = rand()%100 + 1; //Generates random number between 1 and 100
+    //printf("The number is %d\n", number);
+    do{
+        printf("Guess the number between 1 and 100\n");
+        scanf("%d", &guess);
+        if(guess>number){
+        printf("Choose a lower number\n");
+        }
+        else if(number>guess){
+        printf("Choose a higher number\n");
+        }
+        else{ 
+        printf("You guessed the number in %d attempts!\n", nguess);
+        }
+        nguess++;
+    }while(guess!=number);
+return 0;
+}
