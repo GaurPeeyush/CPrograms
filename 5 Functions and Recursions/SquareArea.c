8a15c4baf7746ac17ad8ad4e6fa5143aa7f36ff0
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-09-03 20:30:56 +0530

    Update SquareArea.c

--- 5 Functions and Recursions/SquareArea.c
+++ 5 Functions and Recursions/SquareArea.c
@@ -1,1 +1,9 @@
-
+#include<stdio.h>
+#include<math.h>
+int main(){
+  int side;
+  printf("Enter the value of side:\n");
+  scanf("%d", &side);
+  printf("The area of square is %f", pow(side, 2));
+  return 0;
+}
