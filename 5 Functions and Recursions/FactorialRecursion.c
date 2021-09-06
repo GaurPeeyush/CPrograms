16f5187032098755363ab2f9d7e57a27c13e1004
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-09-06 23:32:35 +0530

    Update FactorialRecursion.c

--- 5 Functions and Recursions/FactorialRecursion.c
+++ 5 Functions and Recursions/FactorialRecursion.c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+int factorial(int x);
+int main(){
+    int n;
+    printf("Enter the integer n:\n");
+    scanf("%d", &n);
+    printf("The value of factorial of %d is %d\n", n, factorial(n));
+    return 0;
+}
+int factorial(int x){
+    if (x==1 || x==0){
+        return 1;
+    }
+    else{
+        return x*factorial(x-1);
+    }
+}
