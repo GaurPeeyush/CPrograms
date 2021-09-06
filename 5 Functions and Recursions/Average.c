e4b90378fd5c0b8fd9db8a6487cd208c96f865ef
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-09-06 23:55:30 +0530

    Update Average.c

--- 5 Functions and Recursions/Average.c
+++ 5 Functions and Recursions/Average.c
@@ -1,1 +1,19 @@
-
+//C program to find average of three numbers using functions
+#include<stdio.h>
+float average (int a, int b, int c);
+int main(){
+    int a, b, c;
+    printf("Enter the value of a\n");
+    scanf("%d", &a);
+    printf("Enter the value of b\n");
+    scanf("%d", &b);
+    printf("Enter the value of c\n");
+    scanf("%d", &c);
+    printf("The average of the three intergers is %f\n", average(a, b, c));
+    return 0;
+}
+float average (int a, int b, int c){
+    float result;
+    result = (a + b + c)/3;
+    return result;
+}
