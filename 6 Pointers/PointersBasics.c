cac47adcda71948d4aecd17f632404a38f3aa809
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-10-08 00:22:20 +0530

    Update PointersBasics.c

--- 6 Pointers/PointersBasics.c
+++ 6 Pointers/PointersBasics.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+int main(){
+  int i = 4;
+  int*j;
+  j = &i; // j will now store the address of i 
+  printf("The value of i is: %d\n", i);
+  printf("The address of i is: %u\n", j);
+  printf("The value of i is: %d\n", *j);
+  printf("The address of i is: %u\n", &i);
+  printf("The value of of j is: %d\n", j);
+  printf("The adress of j is: %u\n", &j);
+  printf("The value of j is: %u\n", *(&j));
+  return 0;
+}
