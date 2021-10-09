2c53e8e897df836ed105cdaef937ddc46cecd507
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-10-10 00:35:51 +0530

    Update PointerToPointer.c

--- 6 Pointers/PointerToPointer.c
+++ 6 Pointers/PointerToPointer.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+int main(){
+    int i=4;
+    int*j;
+    j=&i;
+    int**k;
+    k=&j;
+    printf("The value of i is: %d\n", *j);
+    printf("The address of i is: %u\n", j);
+    printf("The value of j is: %u\n", *(&j));
+    printf("The address of j is: %u\n", k);
+    printf("The value of k is: %u\n", *(&k));
+    return 0;
+}
