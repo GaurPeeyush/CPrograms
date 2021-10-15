dc5392e12f807afc680a8cef6230b2afbb211060
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-10-15 23:50:49 +0530

    Update Problem01.c

--- 6 Pointers/Problem01.c
+++ 6 Pointers/Problem01.c
@@ -1,1 +1,13 @@
-
+//Write a C program having variable i. Print the address of i. 
+//Pass the this variable to a function and print its address. 
+//Compare both the addresses.
+#include<stdio.h>
+void funtion(int a){
+    printf("The address of variable a is %u\n", &a);
+} 
+int main(){
+    int i = 5;
+    printf("The value of i is: %d\n", i);
+    funtion(i);
+    printf("The address of variable i is %u", &i);
+}
