44ece6ab72c9e7d00049cd8b9ea6023fb79f662d
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-10-17 01:24:41 +0530

    Update Problem03.c

--- 6 Pointers/Problem03.c
+++ 6 Pointers/Problem03.c
@@ -1,1 +1,12 @@
-
+//C program to change the value of a variable to 10 times its current value
+//Write a function and pass the value by reference
+#include<stdio.h>
+void fun(int *a, int *b){
+    *b = 100*(*a);
+}
+int main(){
+    int a = 20, b;
+    fun(&a, &b);
+    printf("The value of 10 times of a is %d\n", b);
+    return 0;
+}
