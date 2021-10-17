94cdae3185dd71c0493a5f6b6ef1de4f7616c0a9
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-10-17 20:39:54 +0530

    Update Problem04.c

--- 6 Pointers/Problem04.c
+++ 6 Pointers/Problem04.c
@@ -1,1 +1,11 @@
-
+//Program to print value of variable using pointer to pointer
+#include<stdio.h>
+int main(){
+    int i = 4;
+    int * j; //pointer
+    int ** k; //pointer to pointer
+    j = &i;
+    k = &j;
+    printf("The value of i is: %d", *(*k));
+    return 0;
+}
