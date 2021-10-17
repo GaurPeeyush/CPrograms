a2bce05ece244733ea25e8e085ea07e606e6a2d7
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-10-18 00:36:03 +0530

    Update ArrayInput.c

--- 7 Array/ArrayInput.c
+++ 7 Array/ArrayInput.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+int main(){
+    int marks[4];
+    printf("Enter marks one:\n");
+    scanf("%d", &marks[0]);
+    printf("Enter marks two:\n");
+    scanf("%d", &marks[1]);
+    printf("Enter marks three:\n");
+    scanf("%d", &marks[2]);
+    printf("Enter marks four:\n");
+    scanf("%d", &marks[3]);
+    printf("The marks you have entered are: %d, %d, %d, and %d.", marks[0], marks[1], marks[2], marks[3]);
+    return 0;
+}
