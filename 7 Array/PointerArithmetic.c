76bfa1b9af5e7c5733d8e9940d4c0ff2ef6505d4
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-10-20 00:14:46 +0530

    Update PointerArithmetic.c

--- 7 Array/PointerArithmetic.c
+++ 7 Array/PointerArithmetic.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+int main(){
+    int i = 4; // size of integer is 4 bytes
+    int * ptr;
+    ptr = &i;
+    printf("The value of ptr is %u\n", ptr);
+    ptr++; // or ptr = ptr + 1
+    printf("The value of ptr++ is %u\n", ptr);
+    char c = 45; // size of character is 1 byte
+    char * ptr_ch;
+    ptr_ch = &c;
+    printf("The value of ptr_ch is %u\n", ptr_ch);
+    ptr_ch++;
+    printf("The value of ptr_ch++ is %u\n", ptr_ch);
+    return 0; 
+}
