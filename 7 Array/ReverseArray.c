09685a228e465f48f834b6966b3e2811d4b6bd46
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-10-21 01:24:23 +0530

    Update ReverseArray.c

--- 7 Array/ReverseArray.c
+++ 7 Array/ReverseArray.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main()
+{ 
+    int size;
+    scanf("%d", &size);
+    int arr[size];
+    for(int i=0; i<size; i++){
+        scanf("%d", &arr[i]);
+    }
+    for(int i=(size-1); i>-1; i--){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
