6399186ad0fd60be4d0ebe85f79b6a98a79b309b
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-10-28 23:57:55 +0530

    Update 2-DArrayPrint.c

--- 7 Array/2-DArrayPrint.c
+++ 7 Array/2-DArrayPrint.c
@@ -1,1 +1,20 @@
-
+#include<stdio.h>
+int main(){
+    int i, j;
+    int arr[2][2];
+    for(i=0; i<2; i++){
+        for(j=0; j<2; j++){
+            printf("Enter the values of elements\n");
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for(i=0; i<2; i++){
+        for(j=0; j<2; j++){
+            printf("%d ", arr[i][j]);
+            if(j==1){
+                printf("\n");
+            }
+        }
+    }
+    return 0;
+}
