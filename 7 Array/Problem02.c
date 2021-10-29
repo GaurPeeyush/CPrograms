3ae12ad6c933c3b5011cfb0a86d6bdc7991c3cad
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-10-29 22:45:44 +0530

    Update Problem02.c

--- 7 Array/Problem02.c
+++ 7 Array/Problem02.c
@@ -1,1 +1,11 @@
-
+//Create an array and print table of 5
+#include<stdio.h>
+int main(){
+    int arr[10];
+    for(int i = 1; i<11; i++){
+    arr[i-1] = 5*i; 
+    }
+    for(int i = 0; i<10; i++){
+        printf("5X%d = %d\n", i+1, arr[i]);
+    }
+}
