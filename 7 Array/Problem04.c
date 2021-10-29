56a2daa029bf29ec3a1d6e1022c688efc75e314c
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-10-29 23:26:13 +0530

    Update and rename Problem0.c to Problem04.c

--- 7 Array/Problem04.c
+++ 7 Array/Problem04.c
@@ -1,6 +1,5 @@
 //Program which counts number of +ve intergers in an array
 #include<stdio.h>
-int positive(int *arr);
 int main(){
     int arr[] = {1, 2, -1, 0, -5, -6, 2, 8};
     int sum = 0;
@@ -11,12 +10,4 @@ int main(){
     }
     printf("The total number of +ve integers in the array are %d\n", sum);
     return 0;
-}
-int positive(int *arr){
-    int sum = 0;
-    for(int i=0; i<8; i++){
-        if(arr[i]>0){
-            sum = sum + 1;
-        }
-    }
 }
