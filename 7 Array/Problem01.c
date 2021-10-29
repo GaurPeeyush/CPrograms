5f701914d17ba8106f4e1b0af215c5d9a4246e78
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-10-29 22:33:57 +0530

    Update Problem01.c

--- 7 Array/Problem01.c
+++ 7 Array/Problem01.c
@@ -1,1 +1,14 @@
-
+// Verify that pointer, (ptr+2) points to the third element when ptr points to first one of an array
+#include<stdio.h>
+int main(){
+    int arr[4];
+    int *ptr = &arr[0];
+    ptr = ptr + 2;
+    if(ptr == &arr[2]){
+        printf("They point to same memory location");
+    }
+    else{
+        printf("They do not point to the same memory location");
+    }
+return 0;
+}
