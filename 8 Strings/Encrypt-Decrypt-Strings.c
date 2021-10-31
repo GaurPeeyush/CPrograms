458712cacce8d791ad3de64fbc736159839443d8
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-11-01 00:28:33 +0530

    Update Encrypt-Decrypt-Strings.c

--- 8 Strings/Encrypt-Decrypt-Strings.c
+++ 8 Strings/Encrypt-Decrypt-Strings.c
@@ -1,1 +1,24 @@
-
+#include<stdio.h>
+void encrypt(char *str){
+    char *ptr = str; 
+    while(*ptr!=0){
+        *ptr = *ptr + 1;
+        ptr++;
+    }
+}
+void decrypt(char *str2){
+    char *ptr = str2; 
+    while(*ptr!=0){
+        *ptr = *ptr - 1;
+        ptr++;
+    }
+}
+int main(){
+    char str[] = "My name is Peeyush";
+    char str2[] = "Nz!obnf!jt!Qffzvti";
+    encrypt(str);
+    printf("Encrypted message is: %s\n", str);
+    decrypt(str2);
+    printf("Decrypted message is: %s\n", str2);
+    return 0;
+}
