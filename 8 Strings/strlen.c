fb2eae8e756bbc33852be9026d953465b15bc9ef
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-10-31 22:55:28 +0530

    Create strlen.c

---
+++ 8 Strings/strlen.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char *str = "Peeyush";
+    int length = strlen(str);
+    printf("The length of the string is: %d", length);
+    return 0;
+}
