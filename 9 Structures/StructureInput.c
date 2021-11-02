4bb5353e06b9e150494edbd4fae7b29da3cc9cf9
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-11-02 22:26:12 +0530

    Update StructureInput.c

--- 9 Structures/StructureInput.c
+++ 9 Structures/StructureInput.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+struct employee{
+    char name[50];
+    float salary;
+    int usn;
+};
+int main(){
+    struct employee e1;
+    printf("Enter the name of the employee: \n");
+    gets(e1.name);
+    
+    printf("Enter the name of the salary of the employee: \n");
+    scanf("%f", &e1.salary);
 
+    printf("Enter the USN of the employee: \n");
+    scanf("%d", &e1.usn);
+
+    return 0;
+
+}
